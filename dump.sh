3f63a28f474f916fe0dd94d0a64c7069b357320c
root <root@localhost.localdomain>
2016-07-25 09:10:13 -0400

    dump script

---
+++ dump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" != 2 ] || [ "$1" == "-h" ]
+then
+	echo "$0 directoryDump exefilename"
+	echo "create a directory with RAM dump and a CRIU dump of a process of the executable file exefilename"
+	exit
+fi
+
+totalSize=$(free -mt |grep Total: | awk '{print $2}')
+directory=$1
+elf=$2
+mkdir $directory
+pid=$(ps -C $elf -o pid=)
+echo "Pid found of process $elf: $pid"
+if [ ! -e /dev/fmem ] 
+then
+	(cd /fmem-master; ./run.sh)
+fi
+echo "Dumping RAM memory of $totalSize MB"
+dd if=/dev/fmem of=$directory/$directory.dump bs=1MB count=$totalSize
+mkdir $directory/CRIU
+echo "Dumping process $elf pid $pid"
+criu dump -D $directory/CRIU -t $pid --shell-job
+echo "DONE."
+
