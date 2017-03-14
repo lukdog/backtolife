133cd9cc15c33dfa27bca3aa8f52cccb8d92698e
Luca Doglione <lukdog@users.noreply.github.com>
2017-03-14 08:36:15 +0100

    more cleaning

--- findBytes.py
+++ findBytes.py
@@ -15,21 +15,11 @@ for line in os.popen("xxd "+ sys.argv[1],"r",1):
 	add.append(line[9:48].replace(" ",""))
 	total+=line[9:48].replace(" ","")
 
-merda = str(sys.argv[2])
-if merda in total:
-	riga = int(total.find(merda))/int(32)
+toSearch = str(sys.argv[2])
+if toSearch in total:
+	riga = int(total.find(toSearch))/int(32)
 	if "{0:07x}".format((riga-1)*16) in addresses:
 		print "{0:07x}".format((riga-1)*16)+":" + addresses["{0:07x}".format((riga-1)*16)]
 	print "{0:07x}".format(riga*16)+":" + addresses["{0:07x}".format(riga*16)]
 	if "{0:07x}".format((riga+1)*16) in addresses:
 		print "{0:07x}".format((riga+1)*16)+":" + addresses["{0:07x}".format((riga+1)*16)]
-	
-	
-	
-
-
-
-
-
-		
-		
